5b27227b45f097a5696ccf4d53e3c73f4b5ce78a
camigirl95 <camillehaggard@yahoo.com>
2014-06-23 13:29:59 -0700

    asd
    asd

---
+++ write.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+  
+  </head>
+  <body>
+    <form action="http://jrmeza.comxa.com/clients/camille_haggard/php/createAlbum.php" method="POST">
+      <input name="alb_name">
+      <input type="submit">
+    </form>
+  </body>
+</html>
